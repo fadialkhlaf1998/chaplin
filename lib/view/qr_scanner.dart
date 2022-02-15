e8f74b2d83398ef14f8bdd61df56a566815708a8
Fadi-h <62059908+Fadi-h@users.noreply.github.com>
2022-02-15 13:53:26 +0200

    new version

--- lib/view/qr_scanner.dart
+++ lib/view/qr_scanner.dart
@@ -142,15 +142,16 @@ class _QRScannerState extends State<QRScanner> {
 
   static openwhatsapp(BuildContext context,String msg) async{
     var whatsapp ="+971 52 692 4021";
-    var whatsappURl_android = "whatsapp://send?phone="+whatsapp+"&text=$msg";
-    var whatappURL_ios ="https://wa.me/$whatsapp?text=${Uri.parse(msg)}";
-    if( await canLaunch(whatappURL_ios)){
-      await launch(whatappURL_ios);
-    }else{
-      ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: new Text("whatsapp no installed")));
+    var whatsappURl_android = "https://api.whatsapp.com/send?phone=$whatsapp=${Uri.parse(msg)}";
+    var whatappURL_ios ="https://wa.me/$whatsapp/?text=${Uri.parse(msg)}";
 
-    }
+      // add the [https]
+      if( await canLaunch(whatappURL_ios)){
+        await launch(whatappURL_ios);
+      }else{
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: new Text("whatsapp no installed")));
+      }
   }
 
 
